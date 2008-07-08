svn2github/Shareaza-trunk

commit b19498dffa0ae2c0591460d2dd21eed10bacfe6b

Added Shareaza.Application COM object
git-svn-id: https://svn.code.sf.net/p/shareaza/code/trunk@7104 3231ed4b-2112-0410-84e0-8c3944a40730

shareaza/Plugins.cpp
@@ -407,8 +407,9 @@ BOOL CPlugin::Start()
 		return FALSE;
 	}
 
-	m_pPlugin->SetApplication(
-		(IApplication*)Application.GetInterface( IID_IApplication, FALSE ) );
+	IApplication* pApplication = NULL;
+	if ( SUCCEEDED( CApplication::GetApp( &pApplication ) ) )
+		m_pPlugin->SetApplication( pApplication );
 
 	m_nCapabilities = 0;
 	m_pPlugin->QueryCapabilities( &m_nCapabilities );

shareaza/Application.cpp
@@ -42,6 +42,11 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
+IMPLEMENT_DYNCREATE(CApplication, CComObject)
+
+// {E9B2EF9B-4A0C-451e-801F-257861B87FAD}
+IMPLEMENT_OLECREATE_FLAGS(CApplication, "Shareaza.Application", afxRegFreeThreading|afxRegApartmentThreading, 0xe9b2ef9b, 0x4a0c, 0x451e, 0x80, 0x1f, 0x25, 0x78, 0x61, 0xb8, 0x7f, 0xad);
+
 BEGIN_MESSAGE_MAP(CApplication, CComObject)
 END_MESSAGE_MAP()
 
@@ -50,8 +55,6 @@ BEGIN_INTERFACE_MAP(CApplication, CComObject)
 	INTERFACE_PART(CApplication, IID_IUserInterface, UserInterface)
 	INTERFACE_PART(CApplication, IID_ISettings, Settings)
 END_INTERFACE_MAP()
-
-CApplication Application;
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -71,19 +74,22 @@ CApplication::~CApplication()
 /////////////////////////////////////////////////////////////////////////////
 // CApplication operations
 
-IApplication* CApplication::GetApp()
+HRESULT CApplication::GetApp(IApplication** ppIApplication) throw()
 {
-	return (IApplication*)GetInterface( IID_IApplication, TRUE );
+	return CoCreateInstance( CLSID_ShareazaApplication, NULL, CLSCTX_ALL,
+		IID_IApplication, (void**)ppIApplication );
 }
 
-IUserInterface* CApplication::GetUI()
+HRESULT CApplication::GetUI(IUserInterface** ppIUserInterface) throw()
 {
-	return (IUserInterface*)GetInterface( IID_IUserInterface, TRUE );
+	return CoCreateInstance( CLSID_ShareazaApplication, NULL, CLSCTX_ALL,
+		IID_IUserInterface, (void**)ppIUserInterface );
 }
 
-ISettings* CApplication::GetSettings()
+HRESULT CApplication::GetSettings(ISettings** ppISettings) throw()
 {
-	return (ISettings*)GetInterface( IID_ISettings, TRUE );
+	return CoCreateInstance( CLSID_ShareazaApplication, NULL, CLSCTX_ALL,
+		IID_ISettings, (void**)ppISettings );
 }
 
 /////////////////////////////////////////////////////////////////////////////

shareaza/Application.h
@@ -19,28 +19,20 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 
-#if !defined(APPLICATION_H)
-#define APPLICATION_H
-
 #pragma once
+
 
 class CApplication : public CComObject
 {
-// Construction
+	DECLARE_DYNCREATE(CApplication)
+
 public:
 	CApplication();
 	virtual ~CApplication();
 
-// Attributes
-public:
-
-// Operations
-public:
-	IApplication*	GetApp();
-	IUserInterface*	GetUI();
-	ISettings*		GetSettings();
-
-	DECLARE_MESSAGE_MAP()
+	static HRESULT GetApp(IApplication** ppIApplication) throw();
+	static HRESULT GetUI(IUserInterface** ppIUserInterface) throw();
+	static HRESULT GetSettings(ISettings** ppISettings) throw();
 
 // IApplication
 protected:
@@ -72,13 +64,10 @@ protected:
 
 	BEGIN_INTERFACE_PART(Settings, ISettings)
 		DECLARE_DISPATCH()
-		STDMETHOD(GetValue(VARIANT* value));	// Pass as BSTR path (e.g. Gnutella2.EnableAlways, 
-												// get back the actual value
+		STDMETHOD(GetValue)(VARIANT* value);	// Pass as BSTR path (e.g. Gnutella2.EnableAlways, get back the actual value
 	END_INTERFACE_PART(Settings)
 
+	DECLARE_MESSAGE_MAP()
+	DECLARE_OLECREATE(CApplication)
 	DECLARE_INTERFACE_MAP()
 };
-
-extern CApplication Application;
-
-#endif // !defined(APPLICATION_H)

shareaza/ComMenu.cpp
@@ -83,15 +83,13 @@ IMPLEMENT_DISPATCH(CComMenu, SMenu)
 STDMETHODIMP CComMenu::XSMenu::get_Application(IApplication FAR* FAR* ppApplication)
 {
 	METHOD_PROLOGUE( CComMenu, SMenu )
-	*ppApplication = Application.GetApp();
-	return S_OK;
+	return CApplication::GetApp( ppApplication );
 }
 
 STDMETHODIMP CComMenu::XSMenu::get_UserInterface(IUserInterface FAR* FAR* ppUserInterface)
 {
 	METHOD_PROLOGUE( CComMenu, SMenu )
-	*ppUserInterface = Application.GetUI();
-	return S_OK;
+	return CApplication::GetUI( ppUserInterface );
 }
 
 STDMETHODIMP CComMenu::XSMenu::get__NewEnum(IUnknown FAR* FAR* ppEnum)

shareaza/ComToolbar.cpp
@@ -85,15 +85,13 @@ IMPLEMENT_DISPATCH(CComToolbar, SToolbar)
 STDMETHODIMP CComToolbar::XSToolbar::get_Application(IApplication FAR* FAR* ppApplication)
 {
 	METHOD_PROLOGUE( CComToolbar, SToolbar )
-	*ppApplication = Application.GetApp();
-	return S_OK;
+	return CApplication::GetApp( ppApplication );
 }
 
 STDMETHODIMP CComToolbar::XSToolbar::get_UserInterface(IUserInterface FAR* FAR* ppUserInterface)
 {
 	METHOD_PROLOGUE( CComToolbar, SToolbar )
-	*ppUserInterface = Application.GetUI();
-	return S_OK;
+	return CApplication::GetUI( ppUserInterface );
 }
 
 STDMETHODIMP CComToolbar::XSToolbar::get__NewEnum(IUnknown FAR* FAR* ppEnum)
@@ -174,15 +172,13 @@ IMPLEMENT_DISPATCH(CComToolbar, SToolbarItem)
 STDMETHODIMP CComToolbar::XSToolbarItem::get_Application(IApplication FAR* FAR* ppApplication)
 {
 	METHOD_PROLOGUE( CComToolbar, SToolbarItem )
-	*ppApplication = Application.GetApp();
-	return S_OK;
+	return CApplication::GetApp( ppApplication );
 }
 
 STDMETHODIMP CComToolbar::XSToolbarItem::get_UserInterface(IUserInterface FAR* FAR* ppUserInterface)
 {
 	METHOD_PROLOGUE( CComToolbar, SToolbarItem )
-	*ppUserInterface = Application.GetUI();
-	return S_OK;
+	return CApplication::GetUI( ppUserInterface );
 }
 
 STDMETHODIMP CComToolbar::XSToolbarItem::get_Toolbar(ISToolbar FAR* FAR* ppToolbar)

shareaza/Library.cpp
@@ -611,8 +611,7 @@ STDMETHODIMP CLibrary::XLibrary::get_Application(IApplication FAR* FAR* ppApplic
 {
 	METHOD_PROLOGUE( CLibrary, Library )
 	if ( ppApplication == NULL ) return E_INVALIDARG;
-	*ppApplication = Application.GetApp();
-	return S_OK;
+	return CApplication::GetApp( ppApplication );
 }
 
 STDMETHODIMP CLibrary::XLibrary::get_Library(ILibrary FAR* FAR* ppLibrary)

shareaza/LibraryFolders.cpp
@@ -526,8 +526,7 @@ IMPLEMENT_DISPATCH(CLibraryFolders, LibraryFolders)
 STDMETHODIMP CLibraryFolders::XLibraryFolders::get_Application(IApplication FAR* FAR* ppApplication)
 {
 	METHOD_PROLOGUE( CLibraryFolders, LibraryFolders )
-	*ppApplication = Application.GetApp();
-	return S_OK;
+	return CApplication::GetApp( ppApplication );
 }
 
 STDMETHODIMP CLibraryFolders::XLibraryFolders::get_Library(ILibrary FAR* FAR* ppLibrary)

shareaza/LibraryMaps.cpp
@@ -947,8 +947,7 @@ IMPLEMENT_DISPATCH(CLibraryMaps, LibraryFiles)
 STDMETHODIMP CLibraryMaps::XLibraryFiles::get_Application(IApplication FAR* FAR* ppApplication)
 {
 	METHOD_PROLOGUE( CLibraryMaps, LibraryFiles )
-	*ppApplication = Application.GetApp();
-	return S_OK;
+	return CApplication::GetApp( ppApplication );
 }
 
 STDMETHODIMP CLibraryMaps::XLibraryFiles::get_Library(ILibrary FAR* FAR* ppLibrary)

commit 0576cdd0c75a4d2f8c3724f0adb2a6571afe5735

Fixed COM interface leak (introduced in r7104).
git-svn-id: https://svn.code.sf.net/p/shareaza/code/trunk@7122 3231ed4b-2112-0410-84e0-8c3944a40730

shareaza/Plugins.cpp
@@ -407,7 +407,7 @@ BOOL CPlugin::Start()
 		return FALSE;
 	}
 
-	IApplication* pApplication = NULL;
+	CComPtr< IApplication > pApplication;
 	if ( SUCCEEDED( CApplication::GetApp( &pApplication ) ) )
 		m_pPlugin->SetApplication( pApplication );
 