svn2github/Shareaza-trunk

commit 2951a6dc406031906cb89ca57e33da8f2bc14fc5

Added support for .sd-files (Shareaza Download) opening (also by double click), note that .sd-file re-saved to Incomplete folder but corresponding .partial-files left in the original place. Also improved handling of existing .partial-files when Incomplete folder was moved.
git-svn-id: https://svn.code.sf.net/p/shareaza/code/trunk@9381 3231ed4b-2112-0410-84e0-8c3944a40730

shareaza/FragmentedFile.cpp
@@ -189,66 +189,52 @@ BOOL CFragmentedFile::Open(LPCTSTR pszFile, QWORD nOffset, QWORD nLength, BOOL b
 			return ! bWrite || (*pItr).m_pFile->EnsureWrite();
 	}
 
-	CTransferFile* pFile = NULL;
-	QWORD nRealLength = SIZE_UNKNOWN;
+	// Guess existing path
 	CString& strPath = (*pItr).m_sPath;
-	for ( int nMethod = 0 ; ; ++nMethod )
+	if ( GetFileAttributes( _T("\\\\?\\") + strPath ) == INVALID_FILE_ATTRIBUTES )
 	{
-		pFile = TransferFiles.Open( strPath, bWrite );
-		if ( pFile )
+		const CString strFileName = PathFindFileName( strPath );
+		if ( GetFileAttributes( _T("\\\\?\\") + Settings.Downloads.IncompletePath + _T("\\") + strFileName ) == INVALID_FILE_ATTRIBUTES )
 		{
-			m_nFileError = ERROR_SUCCESS;
-			nRealLength = pFile->GetSize();
-			if ( pFile->IsExists() && nLength == SIZE_UNKNOWN )
+			const CString strSDPath = m_pDownload->m_sPath.Left( m_pDownload->m_sPath.ReverseFind( _T('\\') ) );
+			if ( GetFileAttributes( _T("\\\\?\\") + strSDPath + _T("\\") + strFileName ) == INVALID_FILE_ATTRIBUTES )
 			{
-				nLength = nRealLength;
+				// Use as is
 			}
-			else if ( ! pFile->IsWritable() && nRealLength != nLength )
+			else
 			{
-				// Wrong file
-				pFile->Release();
-				pFile = NULL;
-				m_nFileError = ERROR_FILE_INVALID;
+				strPath = strSDPath + _T("\\") + strFileName;
 			}
-			break;
 		}
-
-		m_nFileError = ::GetLastError();
-		if ( ! bWrite )
-			// Do nothing for read only files
-			break;
-
-		if ( nMethod == 0 )
+		else
 		{
-			// Try to open file from current incomplete folder (in case of changed folder)
-			CString strIncompletePath = Settings.Downloads.IncompletePath + _T("\\") + PathFindFileName( strPath );
-			if ( strIncompletePath.CompareNoCase( strPath ) != 0 )
-			{
-				strPath = strIncompletePath;
-				continue;
-			}
-			++nMethod;
+			strPath = Settings.Downloads.IncompletePath + _T("\\") + strFileName;
 		}
-
-		if ( nMethod == 1 )
-		{
-			// Try to open file from same path as SD-file has
-			if ( m_pDownload )
-			{
-				CString strSdPath = m_pDownload->m_sPath.Left( m_pDownload->m_sPath.ReverseFind( _T('\\') ) + 1 ) + PathFindFileName( strPath );
-				if ( strSdPath.CompareNoCase( strPath ) != 0 )
-				{
-					strPath = strSdPath;
-					continue;
-				}
-			}
-			++nMethod;
-		}
-
-		// TODO: Other methods
-		break;
 	}
 
+	QWORD nRealLength = SIZE_UNKNOWN;
+	CTransferFile* pFile = TransferFiles.Open( strPath, bWrite );
+	if ( pFile )
+	{
+		m_nFileError = ERROR_SUCCESS;
+		nRealLength = pFile->GetSize();
+		if ( pFile->IsExists() && nLength == SIZE_UNKNOWN )
+		{
+			nLength = nRealLength;
+		}
+		else if ( ! pFile->IsWritable() && nRealLength != nLength )
+		{
+			// Wrong file
+			pFile->Release();
+			pFile = NULL;
+			m_nFileError = ERROR_FILE_INVALID;
+		}
+	}
+	else
+	{
+		// File error
+		m_nFileError = ::GetLastError();
+	}
 	(*pItr).m_nSize = nLength;
 	(*pItr).m_pFile = pFile;
 

setup/scripts/main.iss
@@ -1,7 +1,7 @@
 ;
 ; main.iss
 ;
-; Copyright (c) Shareaza Development Team, 2002-2013.
+; Copyright (c) Shareaza Development Team, 2002-2014.
 ; This file is part of SHAREAZA (shareaza.sourceforge.net)
 ;
 ; Shareaza is free software; you can redistribute it
@@ -402,6 +402,7 @@ Root: HKCU; Subkey: "Software\Shareaza"; Flags: dontcreatekey uninsdeletekey
 Root: HKU; Subkey: ".DEFAULT\Software\Shareaza"; Flags: dontcreatekey uninsdeletekey
 Root: HKCU; Subkey: "Software\Microsoft\Windows\CurrentVersion\Run"; ValueName: "Shareaza"; Flags: dontcreatekey uninsdeletevalue
 
+Root: HKLM; Subkey: "SOFTWARE\Classes\.sd"; Flags: dontcreatekey uninsdeletekey
 Root: HKLM; Subkey: "SOFTWARE\Classes\.co"; Flags: dontcreatekey uninsdeletekey
 Root: HKLM; Subkey: "SOFTWARE\Classes\.collection"; Flags: dontcreatekey uninsdeletekey
 Root: HKLM; Subkey: "SOFTWARE\Classes\.emulecollection"; Flags: dontcreatekey uninsdeletekey
@@ -424,6 +425,7 @@ Root: HKLM; Subkey: "SOFTWARE\Classes\shareaza*"; Flags: dontcreatekey uninsdele
 Root: HKLM; Subkey: "SOFTWARE\Classes\Applications\Shareaza.exe"; Flags: dontcreatekey uninsdeletekey
 Root: HKLM; Subkey: "SOFTWARE\Classes\Applications\skin.exe"; Flags: dontcreatekey uninsdeletekey
 
+Root: HKCU; Subkey: "SOFTWARE\Classes\.sd"; Flags: dontcreatekey uninsdeletekey
 Root: HKCU; Subkey: "SOFTWARE\Classes\.co"; Flags: dontcreatekey uninsdeletekey
 Root: HKCU; Subkey: "SOFTWARE\Classes\.collection"; Flags: dontcreatekey uninsdeletekey
 Root: HKCU; Subkey: "SOFTWARE\Classes\.emulecollection"; Flags: dontcreatekey uninsdeletekey
@@ -497,6 +499,7 @@ Root: HKCU; SubKey: "Software\BHO Shareaza"; Flags: deletekey
 Root: HKLM; Subkey: "Software\Shareaza\Shareaza\Capabilities"; ValueType: string; ValueName: "ApplicationDescription"; ValueData: "{cm:reg_apptitle}" ; Flags: createvalueifdoesntexist uninsdeletekey
 
 Root: HKLM; Subkey: "Software\Shareaza\Shareaza\Capabilities\FileAssociations"; ValueType: string; ValueName: ".torrent"; ValueData: "Shareaza.Torrent"; Flags: createvalueifdoesntexist uninsdeletevalue
+Root: HKLM; Subkey: "Software\Shareaza\Shareaza\Capabilities\FileAssociations"; ValueType: string; ValueName: ".sd"; ValueData: "Shareaza.Download"; Flags: createvalueifdoesntexist uninsdeletevalue
 Root: HKLM; Subkey: "Software\Shareaza\Shareaza\Capabilities\FileAssociations"; ValueType: string; ValueName: ".co"; ValueData: "Shareaza.Collection"; Flags: createvalueifdoesntexist uninsdeletevalue
 Root: HKLM; Subkey: "Software\Shareaza\Shareaza\Capabilities\FileAssociations"; ValueType: string; ValueName: ".collection"; ValueData: "Shareaza.Collection"; Flags: createvalueifdoesntexist uninsdeletevalue
 Root: HKLM; Subkey: "Software\Shareaza\Shareaza\Capabilities\FileAssociations"; ValueType: string; ValueName: ".emulecollection"; ValueData: "Shareaza.Collection"; Flags: createvalueifdoesntexist uninsdeletevalue

shareaza/Download.cpp
@@ -1,7 +1,7 @@
 //
 // Download.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2013.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -768,7 +768,7 @@ BOOL CDownload::Load(LPCTSTR pszName)
 
 	BOOL bSuccess = FALSE;
 	CFile pFile;
-	if ( pFile.Open( m_sPath, CFile::modeRead ) )
+	if ( pFile.Open( _T("\\\\?\\") + m_sPath, CFile::modeRead ) )
 	{
 		TRY
 		{
@@ -793,7 +793,7 @@ BOOL CDownload::Load(LPCTSTR pszName)
 		pFile.Close();
 	}
 
-	if ( ! bSuccess && pFile.Open( m_sPath + _T(".sav"), CFile::modeRead ) )
+	if ( ! bSuccess && pFile.Open( _T("\\\\?\\") + m_sPath + _T(".sav"), CFile::modeRead ) )
 	{
 		TRY
 		{
@@ -848,7 +848,7 @@ BOOL CDownload::Save(BOOL bFlush)
 	DeleteFileEx( m_sPath + _T(".sav"), FALSE, FALSE, FALSE );
 
 	CFile pFile;
-	if ( ! pFile.Open( m_sPath + _T(".sav"),
+	if ( ! pFile.Open( _T("\\\\?\\") + m_sPath + _T(".sav"),
 		CFile::modeReadWrite|CFile::modeCreate|CFile::osWriteThrough ) )
 		return FALSE;
 

shareaza/Downloads.cpp
@@ -1,7 +1,7 @@
 //
 // Downloads.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2013.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -522,6 +522,19 @@ bool CDownloads::CheckActive(CDownload* pDownload, int nScope) const
 	}
 
 	return false;
+}
+
+CDownload* CDownloads::FindBySDName(const CString& sSDName) const
+{
+	ASSUME_LOCK( Transfers.m_pSection );
+
+	for ( POSITION pos = GetIterator() ; pos ; )
+	{
+		CDownload* pDownload = GetNext( pos );
+		if ( sSDName.CompareNoCase( PathFindFileName( pDownload->m_sPath ) ) == 0 )
+			return pDownload;
+	}
+	return NULL;
 }
 
 CDownload* CDownloads::FindByPath(const CString& sPath) const
@@ -1180,43 +1193,15 @@ void CDownloads::Load()
 	DownloadGroups.CreateDefault();
 	LoadFromCompoundFiles();
 
+	const CString strRootPath = Settings.Downloads.IncompletePath + _T("\\");
+
 	WIN32_FIND_DATA pFind = {};
-	HANDLE hSearch = FindFirstFile( CString( _T("\\\\?\\") ) + Settings.Downloads.IncompletePath + _T("\\*.sd"), &pFind );
+	HANDLE hSearch = FindFirstFile( _T("\\\\?\\") + strRootPath + _T("*.sd"), &pFind );
 	if ( hSearch != INVALID_HANDLE_VALUE )
 	{
 		do
 		{
-			CString strPath;
-			strPath.Format( _T("%s\\%s"),
-				(LPCTSTR)Settings.Downloads.IncompletePath, pFind.cFileName );
-
-			CDownload* pDownload = new CDownload();
-			if ( pDownload->Load( strPath ) )
-			{
-				if ( pDownload->IsSeeding() )
-				{
-					if ( ! Settings.BitTorrent.AutoSeed )
-					{
-						theApp.Message( MSG_NOTICE, IDS_DOWNLOAD_REMOVE, pDownload->m_sName );
-						DeleteFileEx( strPath, FALSE, TRUE, TRUE );
-						DeleteFileEx( strPath + _T(".sav"), FALSE, FALSE, TRUE );
-						DeleteFileEx( strPath + _T(".png"), FALSE, FALSE, TRUE );
-						continue;
-					}
-					pDownload->m_bComplete = true;
-					pDownload->m_bVerify = TRI_TRUE;
-				}
-				m_pList.AddTail( pDownload );
-			}
-			else
-			{
-				theApp.Message( MSG_ERROR, IDS_DOWNLOAD_REMOVE,
-					( pDownload->m_sName.IsEmpty() ? strPath : pDownload->m_sName ) );
-				DeleteFileEx( strPath, FALSE, TRUE, TRUE );
-				DeleteFileEx( strPath + _T(".sav"), FALSE, FALSE, TRUE );
-				DeleteFileEx( strPath + _T(".png"), FALSE, FALSE, TRUE );
-				delete pDownload;
-			}
+			Load( strRootPath + pFind.cFileName );
 		}
 		while ( FindNextFile( hSearch, &pFind ) );
 
@@ -1226,6 +1211,39 @@ void CDownloads::Load()
 	Save( FALSE );
 	DownloadGroups.Load();
 	Transfers.StartThread();
+}
+
+CDownload* CDownloads::Load(const CString& strPath)
+{
+	ASSUME_LOCK( Transfers.m_pSection );
+
+	CAutoPtr< CDownload > pDownload ( new CDownload() );
+	if ( pDownload->Load( strPath ) )
+	{
+		if ( pDownload->IsSeeding() )
+		{
+			if ( ! Settings.BitTorrent.AutoSeed )
+			{
+				theApp.Message( MSG_NOTICE, IDS_DOWNLOAD_REMOVE, pDownload->m_sName );
+				DeleteFileEx( strPath, FALSE, TRUE, TRUE );
+				DeleteFileEx( strPath + _T(".sav"), FALSE, FALSE, TRUE );
+				DeleteFileEx( strPath + _T(".png"), FALSE, FALSE, TRUE );
+				return NULL;
+			}
+			pDownload->m_bComplete = true;
+			pDownload->m_bVerify = TRI_TRUE;
+		}
+		m_pList.AddTail( pDownload );
+		return pDownload.Detach();
+	}
+	else
+	{
+		theApp.Message( MSG_ERROR, IDS_DOWNLOAD_REMOVE, ( pDownload->m_sName.IsEmpty() ? strPath : pDownload->m_sName ) );
+		DeleteFileEx( strPath, FALSE, TRUE, TRUE );
+		DeleteFileEx( strPath + _T(".sav"), FALSE, FALSE, TRUE );
+		DeleteFileEx( strPath + _T(".png"), FALSE, FALSE, TRUE );
+		return NULL;
+	}
 }
 
 void CDownloads::Save(BOOL bForce)

shareaza/Downloads.h
@@ -1,7 +1,7 @@
 //
 // Downloads.h
 //
-// Copyright (c) Shareaza Development Team, 2002-2012.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -88,6 +88,7 @@ public:
 	BOOL		Reorder(CDownload* pDownload, CDownload* pBefore);
 	QWORD		GetAmountDownloadedFrom(IN_ADDR* pAddress);
 
+	CDownload*	FindBySDName(const CString& sSDName) const;
 	CDownload*	FindByPath(const CString& sPath) const;
 	CDownload*	FindByURN(LPCTSTR pszURN, BOOL bSharedOnly = FALSE) const;
 	CDownload*	FindBySHA1(const Hashes::Sha1Hash& oSHA1, BOOL bSharedOnly = FALSE) const;
@@ -98,7 +99,10 @@ public:
 	CDownload*	FindBySID(DWORD nSerID) const;
 	DWORD		GetFreeSID();
 
+	// Load all available .sd-files from Incomplete folder
 	void		Load();
+	// Load specified .sd-file
+	CDownload*	Load(const CString& strPath);
 	void		Save(BOOL bForce = TRUE);
 	void		OnRun();
 	BOOL		OnPush(const Hashes::Guid& oGUID, CConnection* pConnection);

shareaza/Shareaza.cpp
@@ -31,6 +31,7 @@
 #include "DiscoveryServices.h"
 #include "DlgDeleteFile.h"
 #include "DlgMessage.h"
+#include "Download.h"
 #include "DownloadGroups.h"
 #include "Downloads.h"
 #include "EDClients.h"
@@ -773,6 +774,8 @@ BOOL CShareazaApp::Open(LPCTSTR lpszFileName, BOOL bDoIt, BOOL bDisplay)
 	size_t nLength = _tcslen( lpszFileName );
 	if (      nLength > 8  && ! _tcsicmp( lpszFileName + nLength - 8,  _T(".torrent") ) )
 		return OpenTorrent( lpszFileName, bDoIt );
+	else if ( nLength > 3  && ! _tcsicmp( lpszFileName + nLength - 3,  _T(".sd") ) )
+		return OpenDownload( lpszFileName, bDoIt );
 	else if ( nLength > 3  && ! _tcsicmp( lpszFileName + nLength - 3,  _T(".co") ) )
 		return OpenCollection( lpszFileName, bDoIt );
 	else if ( nLength > 11 && ! _tcsicmp( lpszFileName + nLength - 11, _T(".collection") ) )
@@ -923,6 +926,46 @@ BOOL CShareazaApp::OpenURL(LPCTSTR lpszFileName, BOOL bDoIt, BOOL bSilent)
 		theApp.Message( MSG_NOTICE, IDS_URL_PARSE_ERROR );
 
 	return FALSE;
+}
+
+BOOL CShareazaApp::OpenDownload(LPCTSTR lpszFileName, BOOL bDoIt)
+{
+	if ( ! bDoIt )
+		return TRUE;
+
+	CString strFileName = lpszFileName;
+	GetLongPathName( lpszFileName, strFileName.GetBuffer( MAX_PATH * 2 ), MAX_PATH * 2 );
+	strFileName.ReleaseBuffer();
+
+	const CString strSDName = PathFindFileName( strFileName );
+
+	CSingleLock pLock( &Transfers.m_pSection, TRUE );
+
+	// Check for already loaded file
+	if ( Downloads.FindBySDName( strSDName ) == NULL )
+	{
+		// Load a new one
+		if ( CDownload* pDownload = Downloads.Load( lpszFileName ) )
+		{
+			// Save download to Incomplete folder
+			pDownload->m_sPath = Settings.Downloads.IncompletePath + _T("\\") + strSDName;
+			if ( pDownload->Save( TRUE ) )
+			{
+				// Rename old file
+				::MoveFileEx( _T("\\\\?\\") + strFileName, _T("\\\\?\\") + strFileName + _T(".sav"), MOVEFILE_REPLACE_EXISTING | MOVEFILE_WRITE_THROUGH );
+				
+				theApp.Message( MSG_NOTICE, _T("Download file \"%s\" has been successfully loaded and saved as \"%s\"."), (LPCTSTR)strFileName, (LPCTSTR)pDownload->m_sPath );
+			}
+			else
+				theApp.Message( MSG_ERROR, _T("Failed to save download file \"%s\" as \"%s\"."), (LPCTSTR)strFileName, (LPCTSTR)pDownload->m_sPath );
+		}
+		else
+			theApp.Message( MSG_ERROR, _T("Failed to load download file \"%s\"."), (LPCTSTR)strFileName );
+	}
+	else
+		theApp.Message( MSG_WARNING, _T("Download file already loaded \"%s\"."), (LPCTSTR)strFileName );
+
+	return TRUE;
 }
 
 /////////////////////////////////////////////////////////////////////////////

shareaza/Shareaza.h
@@ -185,6 +185,8 @@ public:
 	BOOL				OpenCollection(LPCTSTR lpszFileName, BOOL bDoIt);
 	// Open URL
 	BOOL				OpenURL(LPCTSTR lpszFileName, BOOL bDoIt, BOOL bSilent = FALSE);
+	// Open Shareaza Download file
+	BOOL				OpenDownload(LPCTSTR lpszFileName, BOOL bDoIt);
 
 	CString				GetWindowsFolder() const;
 	CString				GetProgramFilesFolder() const;

shareaza/ShareazaURL.cpp
@@ -1,7 +1,7 @@
 //
 // ShareazaURL.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2013.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -1310,6 +1310,19 @@ void CShareazaURL::Register(BOOL bRegister, BOOL bOnStartup)
 		UnregisterShellType( CLIENT_NAME_T _T(".Torrent") );
 	}
 
+	if ( bRegister )
+	{
+		RegisterShellType( NULL, CLIENT_NAME_T _T(".Download"), CLIENT_NAME_T _T(" Download File"),
+			_T(".sd"), CLIENT_NAME_T, _T("RAZAFORMAT"), IDR_MAINFRAME );
+		RegisterShellType( _T("Applications\\") CLIENT_NAME_T _T(".exe"), NULL, CLIENT_NAME_T _T(" Download File"),
+			_T(".sd"), CLIENT_NAME_T, _T("RAZAFORMAT"), IDR_MAINFRAME );
+	}
+	else
+	{
+		UnregisterShellType( _T(".sd") );
+		UnregisterShellType( CLIENT_NAME_T _T(".Download") );
+	}
+
 	if (  bRegister )
 	{
 		RegisterShellType( NULL, CLIENT_NAME_T _T(".Collection"), CLIENT_NAME_T _T(" Collection File"),
@@ -1405,12 +1418,12 @@ BOOL CShareazaURL::RegisterShellType(LPCTSTR pszRoot, LPCTSTR pszProtocol, LPCTS
 	}
 	else if ( pszType != NULL )
 	{
-		/*HKEY hKeySupported;
+		HKEY hKeySupported;
 		if ( RegCreateKey( hKey, _T("SupportedTypes"), &hKeySupported ) == ERROR_SUCCESS )
 		{
 			RegSetValueEx( hKeySupported, pszType, 0, REG_NONE, NULL, 0 );
 			RegCloseKey( hKeySupported );
-		}*/
+		}
 
 		RegSetValueEx( hKey, _T("FriendlyAppName"), 0, REG_STRING( CLIENT_NAME_T ) );
 	}

commit 0c99a8c3c238a1d437b46e381896102d08a5856f

Fixed possible crash on file upload when file is somehow inaccessible introduced in r9381.
git-svn-id: https://svn.code.sf.net/p/shareaza/code/trunk@9472 3231ed4b-2112-0410-84e0-8c3944a40730

shareaza/FragmentedFile.cpp
@@ -194,21 +194,21 @@ BOOL CFragmentedFile::Open(LPCTSTR pszFile, QWORD nOffset, QWORD nLength, BOOL b
 	if ( GetFileAttributes( _T("\\\\?\\") + strPath ) == INVALID_FILE_ATTRIBUTES )
 	{
 		const CString strFileName = PathFindFileName( strPath );
-		if ( GetFileAttributes( _T("\\\\?\\") + Settings.Downloads.IncompletePath + _T("\\") + strFileName ) == INVALID_FILE_ATTRIBUTES )
+		const CString strIncompletePath = Settings.Downloads.IncompletePath + _T("\\") + strFileName;
+		if ( GetFileAttributes( _T("\\\\?\\") + strIncompletePath ) != INVALID_FILE_ATTRIBUTES )
 		{
-			const CString strSDPath = m_pDownload->m_sPath.Left( m_pDownload->m_sPath.ReverseFind( _T('\\') ) );
-			if ( GetFileAttributes( _T("\\\\?\\") + strSDPath + _T("\\") + strFileName ) == INVALID_FILE_ATTRIBUTES )
-			{
-				// Use as is
-			}
-			else
-			{
-				strPath = strSDPath + _T("\\") + strFileName;
-			}
+			strPath = strIncompletePath;
 		}
 		else
 		{
-			strPath = Settings.Downloads.IncompletePath + _T("\\") + strFileName;
+			if ( m_pDownload )
+			{
+				const CString strSDPath = m_pDownload->m_sPath.Left( m_pDownload->m_sPath.ReverseFind( _T('\\') ) + 1 ) + strFileName;
+				if ( GetFileAttributes( _T("\\\\?\\") + strSDPath ) != INVALID_FILE_ATTRIBUTES )
+				{
+					strPath = strSDPath;
+				}
+			}
 		}
 	}
 